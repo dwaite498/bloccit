4d69467fec714707f084983e6733452d25832639
David Waite <dwaite498@gmail.com>
2017-01-28 11:03:07 +0000

    seeding-assignment

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,6 +15,19 @@ posts = Post.all
        )
 end
 
+puts "#{Post.count}"
+Post.find_or_create_by!(
+    title: "Title Unique",
+    body: "This is a body paragraph. It has the attributes of a paragraph. These attributes include a sentence structure, punctuation, and words made up of letters."
+    )
+puts "#{Post.count}"
+    
+Comment.find_or_create_by!(
+    post: Post.find_by(title: "Title Unique"),
+    body: "This is a second paragraph that also includes the aspects of a paragraph. It has multiple sentences and more than one punctuation mark!"
+    )
+
+
 puts "seed finished"
 puts "#{Post.count} posts created"
 puts "#{Comment.count} comments created"
