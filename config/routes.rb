b7914ede9bcfc6a0ae4c5dcd2a38647b104d9bcf
David Waite <dwaite498@gmail.com>
2017-02-11 14:17:04 +0000

    topics-posts-checkpoint

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,9 @@
 Rails.application.routes.draw do
-  
-  resources :posts
+  resources :topics do
+    resources :posts, except: [:index]
+  end
 
   get 'about' => 'welcome#about'
-  
-  get 'welcome/contact'
-  
-  get 'welcome/faq'
 
   root 'welcome#index'
 end
