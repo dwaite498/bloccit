6a7dab6c3193bee8a23cc9925d7cd4176684afe0
David Waite <dwaite498@gmail.com>
2017-02-14 20:11:46 +0000

    WIP assignment

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,11 @@ Rails.application.routes.draw do
   end
 
   resources :users, only: [:new, :create]
+  
+  post 'users/confirm' => 'user#confirm'
 
   get 'about' => 'welcome#about'
 
   root 'welcome#index'
 end
+
