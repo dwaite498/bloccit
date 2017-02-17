2a4065a642819ea4c82587007409dc14f8c9fcab
David Waite <dwaite498@gmail.com>
2017-02-17 20:45:13 +0000

    WIP middle of updating seeds section

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -2,8 +2,9 @@ require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
   let(:topic) { Topic.create!(name: RandomData.random_sentence, description: RandomData.random_paragraph) }
-  let(:post) { topic.posts.create!(title: RandomData.random_sentence, body: RandomData.random_paragraph) }
   let(:comment) {Comment.create!(body: "comment body", post: post) }
+  let(:user) { User.create!(name: "Doom Hammer", email: "dhammer@bloccit.com", password: "password") }
+  let(:post) { topic.posts.create!(title: RandomData.random_sentence, body: RandomData.random_paragraph, user: user) }
   
   describe "attributes" do
       it "Has a body attribute" do
