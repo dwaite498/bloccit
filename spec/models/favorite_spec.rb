83e6042803e50bdc6e277d81fe5b97e27dc46efe
David Waite <dwaite498@gmail.com>
2017-02-23 13:12:02 +0000

    WIP migrate not working

--- spec/models/favorite_spec.rb
+++ spec/models/favorite_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Favorite, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:topic) { Topic.create!(name: RandomData.random_sentence, description: RandomData.random_sentence) }
+  let(:user) { User.create!(name: "dhammer", email: "dhammer@bloccit.com", password: "password") }
+  let(:post) { topic.post.create!(title: "RandomData.random_sentence", body: "RandomData.random_sentence", user: user) }
+  let(:favorite) { Favorite.create!(post: post, user: user ) }
+  
+  it { is_expected.to belong_to(:post) }
+  it { is_expected.to belong_to(:user) }
 end
