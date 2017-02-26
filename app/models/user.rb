b916b79d97a8e040326cdad416b9c6095a9d7895
David Waite <dwaite498@gmail.com>
2017-02-26 20:43:13 +0000

    WIP public profiles

--- app/models/user.rb
+++ app/models/user.rb
@@ -19,4 +19,9 @@ class User < ActiveRecord::Base
 
   has_secure_password
   enum role: [:member, :admin, :moderator]
+  
+   def avatar_url(size)
+     gravatar_id = Digest::MD5::hexdigest(self.email).downcase
+     "http://gravatar.com/avatar/#{gravatar_id}.png?s=#{size}"
+   end
 end
