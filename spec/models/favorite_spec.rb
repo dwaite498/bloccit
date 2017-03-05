191fd42accacf9a56d40e802cb31dc193a6c96ee
David Waite <dwaite498@gmail.com>
2017-03-05 22:07:20 +0000

    wiki update complete

--- spec/models/favorite_spec.rb
+++ spec/models/favorite_spec.rb
@@ -1,9 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe Favorite, type: :model do
-  let(:topic) { create(:topic) }
-  let(:user) { create(:user) }
-  let(:post) { create(:post) }
+  let(:topic) { Topic.create!(name: RandomData.random_sentence, description: RandomData.random_sentence) }
+  let(:user) { User.create!(name: "dhammer", email: "dhammer@bloccit.com", password: "password") }
+  let(:post) { topic.posts.create!(title: RandomData.random_sentence, body: RandomData.random_paragraph, user: user) }
   let(:favorite) { Favorite.create!(post: post, user: user) }
   
   it { is_expected.to belong_to(:post) }
